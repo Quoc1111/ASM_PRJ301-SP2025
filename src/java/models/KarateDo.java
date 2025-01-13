349f751770884557be30f7b23b8ba3cda34ccc75
Quoc1111 <quoc11112004@gmail.com>
2025-01-14 05:50:26 +0700

    Update KarateDo.java

--- src/java/models/KarateDo.java
+++ src/java/models/KarateDo.java
@@ -12,4 +12,30 @@ package models;
 public class KarateDo {
     private String shinkata;
     private String gatruse;
+
+    public KarateDo() {
+    }
+
+    public KarateDo(String shinkata, String gatruse) {
+        this.shinkata = shinkata;
+        this.gatruse = gatruse;
+    }
+
+    public String getShinkata() {
+        return shinkata;
+    }
+
+    public void setShinkata(String shinkata) {
+        this.shinkata = shinkata;
+    }
+
+    public String getGatruse() {
+        return gatruse;
+    }
+
+    public void setGatruse(String gatruse) {
+        this.gatruse = gatruse;
+    }
+    
+     //jhjhjhjh
 }
