10f40a3da92785855c2848d1332a90d027667b00
Hikari_Coder <khanhkhoi08@gmail.com>
2025-01-14 05:40:15 +0700

    Create KarateDo.java

---
+++ src/java/models/KarateDo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Admin_Coder
+ */
+public class KarateDo {
+    private String shinkata;
+    private String gatruse;
+}
